943cf6cdc807a8d9711a4e76c5dab8ec2e27714e
Philip van Oosten <pvoosten@gmail.com>
2019-10-23 19:40:15 +0200

    Create voorbeelden_ddl.sql

---
+++ voorbeelden_ddl.sql
@@ -0,0 +1,30 @@
+-- select * from TweedeTabel
+--INSERT INTO TweedeTabel (
+--	Id
+--	,Naam
+--	)
+--VALUES (
+--164, 
+--'honderdvierenzestig')
+
+
+--insert into TweedeTabel(Id, Naam)
+--values(45, NULL)
+
+
+-- select * from TweedeTabel
+
+--UPDATE TweedeTabel
+--SET Naam = 'Bart'
+--WHERE Id = 45
+
+
+--UPDATE TweedeTabel
+--SET Naam = 'Bart'
+--WHERE 1=0
+
+INSERT INTO TweedeTabel (Naam, Id)
+VALUES ('Thomas', 1982)
+
+
+
