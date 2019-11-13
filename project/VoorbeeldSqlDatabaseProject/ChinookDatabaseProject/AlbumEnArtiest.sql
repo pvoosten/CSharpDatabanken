3bdfdd6fd7e2f6eaaee1512128bf063daa461bc4
Philip van Oosten <pvoosten@gmail.com>
2019-11-13 20:09:19 +0100

    Update AlbumEnArtiest.sql

--- project/VoorbeeldSqlDatabaseProject/ChinookDatabaseProject/AlbumEnArtiest.sql
+++ project/VoorbeeldSqlDatabaseProject/ChinookDatabaseProject/AlbumEnArtiest.sql
@@ -4,3 +4,13 @@ SELECT al.Title as Album, ar.Name as Artiest
 FROM Album al
 JOIN Artist ar
 	ON al.ArtistId = ar.ArtistId
+	
+-- Bovenstaand statement maakt een view aan
+-- Een view is een schema-object en blijft bestaan in de databank tot `DROP AlbumsPerArtiest` wordt uitgevoerd
+-- Je kan SELECT uitvoeren op een view, net zoals op een tabel
+-- Als je `SELECT AlbumsPerArtiest` uitvoert, wordt het select statement uit de CREATE VIEW opnieuw uitgevoerd
+-- Dus wijzigingen aan de tabellen waarvan gebruik wordt gemaakt zijn onmiddellijk zichtbaar.
+
+-- Waarom views maken?
+-- >> Overzicht te bewaren
+-- >> Om toegang tot de database in detail te beheren.
